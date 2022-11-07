784c31067150387f141bab6bfe2529d49d9dafe5
Danil Gribanov <gribanovwork@yandex.ru>
2022-11-08 02:12:10 +0500

    Сделал слайдер и табы

---
+++ 12_PW/scripts/slider.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  loop: false,
+  spaceBetween: -160,
+  // пагинация
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
